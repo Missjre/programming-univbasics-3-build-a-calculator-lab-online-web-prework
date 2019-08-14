de5aa6af749a1f6908b0b796738403b6dd2a6d0d
Jalysa Reason <jalysareason@gmail.com>
2019-08-14 00:05:55 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+      5 + 5 = 10
 end
 
 def subtraction(num1, num2)
-
+      8 - 1 = 7
 end
 
 def division(num1, num2)
 
+      20 / 10 = 2
 end
-
-def multiplication(num1, num2)
-
+def multiplication(num1, num2) 
+      5 * 10 = 50
 end
 
 def modulo(num1, num2)
-
+      34, 5 = 4
 end
 
 def square_root(num)
-
+      (81) = 9
 end
